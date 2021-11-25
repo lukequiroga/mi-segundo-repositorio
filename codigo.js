a250b4baa96b35f326241011fce64411ad87f244
lucas <luquiroga420@gmail.com>
2021-11-25 13:40:59 -0300

    mi segundo commit

---
+++ codigo.js
@@ -0,0 +1,61 @@
+// GIT COMANDOS BASICOS //
+
+/*  git init
+    git add <file>
+    git status
+    git commit
+    git push
+    git pull
+    git clone  */
+
+    /* iniciando git en una carpeta del escritorio (ya con index.html y js)
+
+    abrimos git.bash y ponemos:
+
+    - pwd (para que nos muestre las opciones de escritorio)
+    - cd 'nombre de la carpeta a la cual queremos entrar'
+    - ls (para que nos muestre el contenido adentro de la carpeta)
+    - git init (para avisarle que vamos a iniciar el sistema de control de versiones)
+
+    // a continuación en la carpeta que iniciamos git nos va a aparecer una carpeta oculta llamada .git (donde no tenemos que tocar nada) //
+
+    - git status (para ver que archivos tenemos o cuales estamos trabajando y tambien ver que archivos estan en el aire, que todavia no los agregamos al area de trabajo)
+    - git add (index.html o codigo.js) (para agregar al area de trabajo y decirle que esos archivos o un archivo conforma la primer versión de mi programa)
+    
+    // si ponemos git status devuelta, vamos a ver que ya estamos trabajando con los archivos por ende nos aparecerian en verde y no en rojo //
+
+//---------------------------------------------------------------------------------------------------------//
+
+//Mi primer reposito//
+
+//para limpiar la terminal usaremos 'cls'
+
+- abrimos terminal en VSC (ya teniendo index.html etc)
+- git config --global user.name "lucas" (para registrarnos - entre comillas iria nuestro nombre)
+
+// si ponemos git config --global user.name, sin nada mas nos aparecia nuestro nombre registrado //
+
+- git config --global user.email "luquiroga420@gmail.com" (email de github)
+
+// si ponemos git config --global user.email, sin nada mas nos aparecia nuestro email registrado //
+
+-git init (para empezara  trabajar con este proyecto)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
